1145cd9d2dc7a2066c30e19dcd5a2848d67a2ce7
Chanin Maiprom <6210110088@psu.ac.th>
2021-07-15 10:03:13 +0700

    go

--- components/Weather.js
+++ components/Weather.js
@@ -1,8 +1,17 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React, { useState } from 'react'
+import { ImageBackground, Text } from 'react-native'
+import Forecast from './Forecast'
 
 export default function Weather(props){
+    const [forecastInfo, setForecastInfo] = useState({
+      main: '-',
+      description: '-',
+      temp: 0
+    })
+
     return(
-        <Text>{props.zipCode}</Text>
+        <ImageBackground source={require('')}>
+        <Forecast {...forecastInfo}/>
+        </ImageBackground>
     )
 }
