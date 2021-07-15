003852920fe5dbe35aac0495a8dbd0794ca17063
Chanin Maiprom <6210110088@psu.ac.th>
2021-07-15 12:49:05 +0700

    go

--- screen/ZipCodeScreen.js
+++ screen/ZipCodeScreen.js
@@ -9,12 +9,12 @@ const availableZipItems = [
     { place: 'Chonburi', code: '20000' },
 ]   
 
-const ZipItem = (place, code) => {
+const ZipItem = ({place, code}) => (
     <View>
         <Text>{place}</Text>
         <Text>{code}</Text>
     </View>
-}
+)
 
 export default function ZipCodeScreen(){
     return (
